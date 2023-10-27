c94a89efeffa72afd69a487cee6e70840698f78e
Marvin Fordan <marvinfordan08@gmail.com>
2023-10-27 20:22:28 +0800

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show, Hide } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App() {
