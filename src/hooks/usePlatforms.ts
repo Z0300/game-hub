d40d358c832a6b1c51a09e87e70a0242190a80f5
Marvin Fordan <marvinfordan08@gmail.com>
2023-11-21 21:16:43 +0800

    Refactoring- create lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,8 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
-import { useQuery } from "@tanstack/react-query";
 
-const apiClient = new APIClient<Platform>("/platforms/lists/paretns");
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
 export interface Platform {
   id: number;
@@ -15,7 +15,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: platforms.length, results: platforms },
+    initialData: platforms,
   });
 
 export default usePlatforms;
